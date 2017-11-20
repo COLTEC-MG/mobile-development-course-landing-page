cca19c5688ae8e031b79d956994982c49c881fbb
João Eduardo Montandon <edu.montandon@gmail.com>
2017-11-20 12:55:55 -0200

    Updated e-mail contact.

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
-$receiver = "edumontandon@gmail.com";
+$receiver = "secgeral@coltec.ufmg.br";
 
 /* All form fields are automatically passed to the PHP script through the array $HTTP_POST_VARS. */
 $email = $_POST['email'];
